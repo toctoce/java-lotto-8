e000df7040b0feefb22522ab6c66da6e853003e6
toctoce <toctoce@naver.com>
2025-10-29 21:24:13 +0900

    feat(InputView): 사용자 입력을 담당하는 클래스 구현

---
+++ src/main/java/lotto/domain/view/InputView.java
@@ -0,0 +1,20 @@
+package lotto.domain.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.message.ViewMessage;
+
+public class InputView {
+    public String readBudget() {
+        System.out.println(ViewMessage.BUDGET_INPUT_MESSAGE.getMessage());
+        return Console.readLine();
+    }
+    public String readWinningLottoNumbers() {
+        System.out.println(ViewMessage.WINNING_LOTTO_NUMBERS_INPUT_MESSAGE.getMessage());
+        return Console.readLine();
+    }
+    public String readWinningLottoBonusNumber() {
+        System.out.println(ViewMessage.WINNING_LOTTO_BONUS_NUMBER_INPUT_MESSAGE.getMessage());
+        return Console.readLine();
+    }
+}
+
