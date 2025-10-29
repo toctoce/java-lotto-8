c9b0055f9a0a554bd701866c14c1966ad7e1e743
toctoce <toctoce@naver.com>
2025-10-29 23:19:35 +0900

    feat(OutputView): 출력 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,27 @@
+package lotto.view;
+
+import lotto.message.ViewMessage;
+
+public class OutputView {
+    // TODO : 파라미터를 dto로 수정해야함.
+    public void writeLottoTicketCount(int lottoTicketCount) {
+        System.out.println(ViewMessage.LOTTO_TICKET_COUNT_OUTPUT_MESSAGE.getMessage(Integer.toString(lottoTicketCount))
+                + ViewMessage.NEW_LINE.getMessage());
+    }
+
+    // TODO : 파라미터로 결과 출력.
+    public void writeDrawLottoResult() {
+        System.out.println(ViewMessage.DRAW_LOTTO_RESULT_OUTPUT_MESSAGE.getMessage());
+        System.out.println(ViewMessage.DIVIDER.getMessage());
+//        System.out.println(ViewMessage.MATCH_3_MESSAGE.getMessage(), match3PrizeMoney, match3Count);
+//        System.out.println(ViewMessage.MATCH_4_MESSAGE.getMessage(), match4PrizeMoney, match4Count);
+//        System.out.println(ViewMessage.MATCH_5_MESSAGE.getMessage(), match5PrizeMoney, match5Count);
+//        System.out.println(ViewMessage.MATCH_5_AND_BONUS_MESSAGE.getMessage(), match5AndBonusPrizeMoney, match5AndBonusCount);
+//        System.out.println(ViewMessage.MATCH_6_MESSAGE.getMessage(), match6PrizeMoney, match6Count);
+    }
+
+    // TODO : 파라미터를 dto로 수정해야함.
+    public void writeRateOfReturn(Double rateOfReturn) {
+        System.out.println(ViewMessage.RATE_OF_RETURN_OUTPUT_MESSAGE.getMessage(rateOfReturn.toString()));
+    }
+}
