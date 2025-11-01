f3da9179f7322de73fdb67af785816b9928ab415
toctoce <toctoce@naver.com>
2025-11-01 16:39:19 +0900

    feat(ErrorMessage): 에러 메시지 enum으로 구현
    간단한 에러 메시지를 1차로 작성함.

---
+++ src/main/java/lotto/domain/message/ErrorMessage.java
@@ -0,0 +1,37 @@
+package lotto.domain.message;
+
+// TODO: 에러 구현해서 [ERROR] 문자열 제거
+public enum ErrorMessage {
+    NOT_A_NUMBER("[ERROR] 숫자만 입력해주세요."),
+    INPUT_REQUIRED("[ERROR] 값을 반드시 입력해야 합니다."),
+
+    AMOUNT_NOT_POSITIVE("[ERROR] 금액은 양수여야 합니다."),
+    AMOUNT_NOT_MULTIPLE_OF_LOTTO_PRICE("[ERROR] 금액이 %s의 배수가 아닙니다", true),
+
+    INVALID_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
+    INVALID_NUMBER_RANGE("[ERROR] 번호가 1이상 45이하여야 합니다."),
+    ;
+    private final String message;
+    private boolean isFormatted;
+
+    ErrorMessage(String message) {
+        this.message = message;
+        this.isFormatted = false;
+    }
+
+    ErrorMessage(String message, boolean isFormatted) {
+        this.message = message;
+        this.isFormatted = isFormatted;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getMessage(String... arguments) {
+        if (isFormatted) {
+            return String.format(message, arguments);
+        }
+        return message;
+    }
+}
