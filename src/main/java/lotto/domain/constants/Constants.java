01b84d2313e3559e5d3dea87917148a5a9805477
toctoce <toctoce@naver.com>
2025-11-01 17:01:27 +0900

    feat(Constants): 상수를 저장하는 파일 생성

---
+++ src/main/java/lotto/domain/constants/Constants.java
@@ -0,0 +1,12 @@
+package lotto.domain.constants;
+
+public class Constants {
+
+    // 객체 생성 방지
+    private Constants() {}
+
+    public static final int RANGE_MIN = 1;
+    public static final int RANGE_MAX = 45;
+
+    public static final int LOTTO_PRICE = 1000;
+}
