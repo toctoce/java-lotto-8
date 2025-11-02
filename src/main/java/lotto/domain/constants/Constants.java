f22a622499aa67b8fc10daaf89d2bcacc4083bb8
toctoce <toctoce@naver.com>
2025-11-02 20:25:52 +0900

    style(Constants): 생성자를 맨 아래로 내림

--- src/main/java/lotto/domain/constants/Constants.java
+++ src/main/java/lotto/domain/constants/Constants.java
@@ -2,12 +2,14 @@ package lotto.domain.constants;
 
 public class Constants {
 
-    // 객체 생성 방지
-    private Constants() {}
+    public static final String DELIMITER = ",";
 
     public static final int LOTTO_NUMBER_MIN = 1;
     public static final int LOTTO_NUMBER_MAX = 45;
     public static final int LOTTO_NUMBER_COUNT = 6;
 
     public static final int LOTTO_PRICE = 1000;
+
+    // 객체 생성 방지
+    private Constants() {}
 }
