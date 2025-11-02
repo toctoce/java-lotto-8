cea1e8fb63f6a6a42b9bff1ab7a7f86a41303ae9
toctoce <toctoce@naver.com>
2025-11-02 20:35:39 +0900

    feat(Parser): 번호 입력 기능 구현
    기능 및 테스트를 구현함.

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -1,14 +1,53 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+import lotto.domain.constants.Constants;
 import lotto.domain.message.ErrorMessage;
 import lotto.domain.vo.Budget;
 
 public class Parser {
+
+    private static final Pattern INTEGER_PATTERN = Pattern.compile("-?\\d+");
+    private static final Pattern NUMBERS_PATTERN = Pattern.compile("[\\d+,]*\\d+");
+
     public static Budget InputToBudget(String input) {
         validateNotBlank(input);
         validateInteger(input);
         int amount = Integer.parseInt(input);
         return new Budget(amount);
+    }
+
+    public static List<Integer> inputToNumbers(String input) {
+        validateNotBlank(input);
+        validateFormat(input, NUMBERS_PATTERN);
+        String[] splitResult = split(input, Constants.DELIMITER);
+        validateNumberCount(splitResult);
+        return splitResultToNumberList(splitResult);
+    }
+
+    private static List<Integer> splitResultToNumberList(String[] splitResult) {
+        return Arrays.stream(splitResult)
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    private static String[] split(String input, String delimiter) {
+        try {
+            String[] splitResult = input.split(delimiter);
+            return splitResult;
+        } catch (PatternSyntaxException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_FORMAT.getMessage());
+        }
+    }
+
+    private static void validateNumberCount(String[] splitResult) {
+        if (splitResult.length != Constants.LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_COUNT.getMessage());
+        }
     }
 
     private static void validateInteger(String input) {
@@ -24,4 +63,12 @@ public class Parser {
             throw new IllegalArgumentException(ErrorMessage.INPUT_REQUIRED.getMessage());
         }
     }
+
+    private static void validateFormat(String input, Pattern pattern) {
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_FORMAT.getMessage());
+        }
+    }
+
 }
--- src/test/java/lotto/util/ParserTest.java
+++ src/test/java/lotto/util/ParserTest.java
@@ -3,15 +3,18 @@ package lotto.util;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 import lotto.domain.constants.Constants;
 import lotto.domain.message.ErrorMessage;
 import lotto.domain.vo.Budget;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -66,7 +69,55 @@ class ParserTest {
                             Integer.toString(Constants.LOTTO_PRICE)
                     ));
         }
+    }
 
+    @ParameterizedTest
+    @MethodSource("generateNumbersData")
+    void 번호입력_정상동작(String input, List<Integer> expected) {
+        // when
+        List<Integer> numbers = Parser.inputToNumbers(input);
+        // then
+        assertThat(numbers).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> generateNumbersData() {
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6", Arrays.asList(1, 2, 3, 4, 5, 6)),
+                Arguments.of("10,20,30,40,41,45", Arrays.asList(10, 20, 30, 40, 41, 45)),
+                Arguments.of("31,32,33,34,35,36", Arrays.asList(31, 32, 33, 34, 35, 36))
+        );
+    }
+
+    @Nested
+    @DisplayName("번호입력 예외케이스")
+    class InputToNumbersTest {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"a,b,c,d,e,f", " , , , , , ", "1,2,3,4,5,x", "1, 2, 3, 4, 5, 6"})
+        void 숫자_쉼표_이외의_다른_문자가_입력된다(String input) {
+            // when, then
+            assertThatThrownBy(() -> Parser.inputToNumbers(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_INPUT_FORMAT.getMessage());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"1", "1,2", "1,2,3", "1,2,3,4", "1,2,3,4,5"})
+        void 로또_숫자는_6개여야_한다(String input) {
+            // when, then
+            assertThatThrownBy(() -> Parser.inputToNumbers(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ErrorMessage.INVALID_NUMBER_COUNT.getMessage());
+        }
+//        TODO: 숫자 범위 검증을 어디서 할지 고민
+//        @ParameterizedTest
+//        @ValueSource(strings = {"0,1,2,3,4,5", "1,2,3,4,5,46", "-1,0,1,2,3,4"})
+//        void 로또_숫자는_1이상_45이하여야_한다(String input) {
+//            // when, then
+//            assertThatThrownBy(() -> Parser.inputToNumbers(input))
+//                    .isInstanceOf(IllegalArgumentException.class)
+//                    .hasMessage(ErrorMessage.INVALID_NUMBER_RANGE.getMessage());
+//        }
     }
 
 }
