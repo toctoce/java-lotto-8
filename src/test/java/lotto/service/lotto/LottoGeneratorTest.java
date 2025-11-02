cb19d66452c1ed59f8200b61f92ccc1752822f96
toctoce <toctoce@naver.com>
2025-11-02 18:10:18 +0900

    refactor(LottoGeneratorTest): 테스트 오류 해결
    파라미터가 LottoCount로 수정된 것을 반영함.

--- src/test/java/lotto/service/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/service/lotto/LottoGeneratorTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import lotto.domain.lotto.Lotto;
 import lotto.domain.lotto.Lottos;
 import lotto.domain.vo.Budget;
+import lotto.domain.vo.LottoCount;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -24,7 +25,7 @@ class LottoGeneratorTest {
     @CsvSource(value = {"1000,1", "5000,5", "10000,10", "200000000,200000"})
     void 로또_모두_생성(int amount, int count) {
         // given, when
-        Lottos lottos = LottoGenerator.createLottos(new Budget(amount));
+        Lottos lottos = LottoGenerator.createLottos(LottoCount.of(new Budget(amount)));
 
         // then
         assertThat(lottos).isNotNull();
