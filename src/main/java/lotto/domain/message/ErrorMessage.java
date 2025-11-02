508fe0edb2c1536e41fa0091ed2c57c4391be14c
toctoce <toctoce@naver.com>
2025-11-02 20:26:46 +0900

    refactor(ErrorMessage): 에러메시지에 상수값 적용

--- src/main/java/lotto/domain/message/ErrorMessage.java
+++ src/main/java/lotto/domain/message/ErrorMessage.java
@@ -1,15 +1,19 @@
 package lotto.domain.message;
 
+import lotto.domain.constants.Constants;
+
 // TODO: 에러 구현해서 [ERROR] 문자열 제거
 public enum ErrorMessage {
     NOT_A_NUMBER("[ERROR] 숫자만 입력해주세요."),
     INPUT_REQUIRED("[ERROR] 값을 반드시 입력해야 합니다."),
+    INVALID_INPUT_FORMAT("[ERROR] 입력 형식이 올바르지 않습니다."),
 
     AMOUNT_NOT_POSITIVE("[ERROR] 금액은 양수여야 합니다."),
     AMOUNT_NOT_MULTIPLE_OF_LOTTO_PRICE("[ERROR] 금액이 %s의 배수가 아닙니다", true),
 
-    INVALID_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
-    INVALID_NUMBER_RANGE("[ERROR] 번호가 1이상 45이하여야 합니다."),
+    INVALID_NUMBER_COUNT("[ERROR] 로또 번호는 " + Constants.LOTTO_NUMBER_COUNT + "개여야 합니다."),
+    INVALID_NUMBER_RANGE("[ERROR] 번호가 " + Constants.LOTTO_NUMBER_MIN + "이상 "
+            + Constants.LOTTO_NUMBER_MAX + "이하여야 합니다."),
 
     LOTTO_COUNT_NOT_POSITIVE("[ERROR] 로또 개수는 양수여야 합니다."),
     ;
