bc4ddd9702353a5a5b167341f04055110e05ecc4
toctoce <toctoce@naver.com>
2025-11-02 17:29:30 +0900

    feat(LottoGenerator): 로또 번호 생성기 구현
    로또 하나를 생성하는 private 함수 구현함.
    로또를 Budget 만큼 생성하는 public 함수 구현함.

---
+++ src/main/java/lotto/service/lotto/LottoGenerator.java
@@ -0,0 +1,34 @@
+package lotto.service.lotto;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import lotto.domain.constants.Constants;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.Lottos;
+import lotto.domain.vo.Budget;
+import lotto.util.RandomNumberGenerator;
+
+public class LottoGenerator {
+    public static Lotto createLotto() {
+        List<Integer> lottoNumbers = RandomNumberGenerator.generateNumbers(
+                Constants.LOTTO_NUMBER_MIN,
+                Constants.LOTTO_NUMBER_MAX,
+                Constants.LOTTO_NUMBER_COUNT);
+
+        return new Lotto(lottoNumbers);
+    }
+
+    public static Lottos createLottos(Budget budget) {
+        int lottoCount = budget.calculateLottoCount();
+
+        List<Lotto> lottos = IntStream.range(0, lottoCount)
+                .mapToObj(i -> createLotto())
+                .toList();
+
+        return new Lottos(lottos);
+    }
+}
+
+
+
---
+++ src/test/java/lotto/service/lotto/LottoGeneratorTest.java
@@ -0,0 +1,34 @@
+package lotto.service.lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.Lottos;
+import lotto.domain.vo.Budget;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class LottoGeneratorTest {
+
+    @Test
+    void 로또_생성() {
+        // given, when
+        Lotto lotto = LottoGenerator.createLotto();
+
+        // then
+        assertThat(lotto).isNotNull();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1000,1", "5000,5", "10000,10", "200000000,200000"})
+    void 로또_모두_생성(int amount, int count) {
+        // given, when
+        Lottos lottos = LottoGenerator.createLottos(new Budget(amount));
+
+        // then
+        assertThat(lottos).isNotNull();
+        assertThat(lottos.getLottosCount()).isEqualTo(count);
+    }
+
+}
