09902d36402a5b3cd8ee609dd73988b7045f4893
toctoce <toctoce@naver.com>
2025-11-02 17:26:55 +0900

    refactor(Constants): 변수명 수정

--- src/main/java/lotto/domain/constants/Constants.java
+++ src/main/java/lotto/domain/constants/Constants.java
@@ -5,8 +5,9 @@ public class Constants {
     // 객체 생성 방지
     private Constants() {}
 
-    public static final int RANGE_MIN = 1;
-    public static final int RANGE_MAX = 45;
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
 
     public static final int LOTTO_PRICE = 1000;
 }
