28c71bb0aee7ec9e05b1b75a5a0fca4e8ef2e9ce
toctoce <toctoce@naver.com>
2025-11-03 15:02:54 +0900

    feat(Constants): 로또 당첨금 작성

--- src/main/java/lotto/domain/constants/Constants.java
+++ src/main/java/lotto/domain/constants/Constants.java
@@ -10,6 +10,13 @@ public class Constants {
 
     public static final int LOTTO_PRICE = 1000;
 
+    public static final int FIRST_PRIZE = 2_000_000_000;
+    public static final int SECOND_PRIZE = 30_000_000;
+    public static final int THIRD_PRIZE = 1_500_000;
+    public static final int FOURTH_PRIZE = 50_000;
+    public static final int FIFTH_PRIZE = 5_000;
+    public static final int NO_PRIZE = 0;
+
     // 객체 생성 방지
     private Constants() {}
 }
