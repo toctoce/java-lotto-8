5f7ab0b7b171382b222525a91059cf99c33976c1
toctoce <toctoce@naver.com>
2025-11-03 19:12:27 +0900

    feat(Application): 메인 함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoGenerator;
+import lotto.service.LottoResultGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(
+                new InputView(),
+                new OutputView(),
+                new LottoGenerator(),
+                new LottoResultGenerator());
+        lottoController.run();
     }
 }
