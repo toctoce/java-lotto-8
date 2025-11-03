9d9ed39108cc679d299ff9e9a87eebe85a5c3d0c
toctoce <toctoce@naver.com>
2025-11-03 17:01:04 +0900

    refactor(Budget): 필요 없는 함수 제거

--- src/main/java/lotto/domain/vo/Budget.java
+++ src/main/java/lotto/domain/vo/Budget.java
@@ -4,7 +4,6 @@ import lotto.domain.constants.Constants;
 import lotto.domain.message.ErrorMessage;
 
 public record Budget(int amount) {
-
     public Budget {
         if (amount <= 0) {
             throw new IllegalArgumentException(ErrorMessage.AMOUNT_NOT_POSITIVE.getMessage());
@@ -14,9 +13,5 @@ public record Budget(int amount) {
                     ErrorMessage.AMOUNT_NOT_MULTIPLE_OF_LOTTO_PRICE
                             .getMessage(Integer.toString(Constants.LOTTO_PRICE)));
         }
-    }
-
-    public int calculateLottoCount() {
-        return amount / Constants.LOTTO_PRICE;
     }
 }
