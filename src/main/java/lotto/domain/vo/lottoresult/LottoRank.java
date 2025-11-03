a39bf9d67eb8c996c395700ba4ef39028c5e4b51
toctoce <toctoce@naver.com>
2025-11-03 13:00:30 +0900

    feat(LottoRank): 로또 등수 enum 구현
    맞은 개수, 보너스 맞았는지 여부로 LottoRank를 생성하는 valueOf 메소드 구현함.

---
+++ src/main/java/lotto/domain/vo/lottoresult/LottoRank.java
@@ -0,0 +1,42 @@
+package lotto.domain.vo.lottoresult;
+
+import java.util.Arrays;
+import lotto.domain.constants.Constants;
+
+public enum LottoRank {
+
+    FIRST(6, false, Constants.FIRST_PRIZE),
+    SECOND(5, true, Constants.SECOND_PRIZE),
+    THIRD(5, false, Constants.THIRD_PRIZE),
+    FOURTH(4, false, Constants.FOURTH_PRIZE),
+    FIFTH(3, false, Constants.FIFTH_PRIZE),
+    MISS(0, false, Constants.NO_PRIZE);
+
+    private final int matchCount;
+    private final boolean matchBonus;
+    private final long prize;
+
+    LottoRank(int matchCount, boolean matchBonus, long prize) {
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
+        this.prize = prize;
+    }
+
+    public static LottoRank valueOf(int matchCount, boolean matchBonus) {
+        if (matchCount == 5) {
+            if (matchBonus) {
+                return SECOND;
+            }
+            return THIRD;
+        }
+
+        return Arrays.stream(LottoRank.values())
+                .filter(rank -> rank.matchCount == matchCount)
+                .findFirst()
+                .orElse(MISS);
+    }
+
+    public long getPrize() {
+        return prize;
+    }
+}
