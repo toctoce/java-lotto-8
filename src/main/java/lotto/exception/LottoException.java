5a1d5367ad0034946b9075779c942f1f101b45ad
toctoce <toctoce@naver.com>
2025-11-03 20:12:44 +0900

    feat(LottoException): 에러 클래스 생성

---
+++ src/main/java/lotto/exception/LottoException.java
@@ -0,0 +1,16 @@
+package lotto.exception;
+
+import lotto.message.ErrorMessage;
+
+public class LottoException extends IllegalArgumentException {
+
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
+    public LottoException(ErrorMessage errorMessage) {
+        super(formatMessage(errorMessage.getMessage()));
+    }
+
+    private static String formatMessage(String message) {
+        return ERROR_PREFIX + message;
+    }
+}
